84f9479b0c3e5123e789c878bcae233e769ae7de
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-25 12:29:52 +0530

    Doing same using async and await

--- Program.cs
+++ Program.cs
@@ -5,30 +5,41 @@ class MyClass
 {
     static void Main(string[] args)
     {
-        Calculate();
+        Test();
         Console.ReadLine();
     }
-
-    private static void Calculate()
+    async static Task Test()
     {
-        Task<int> task1 = Task.Run(() =>
-        {
-            return Calculate1();
+        await Calculate();
+    }
+    private async static Task Calculate()
+    {
+        //Task<int> task1 = Task.Run(() =>
+        //{
+        //    return Calculate1();
 
-        });
+        //});
 
-        TaskAwaiter<int> awaiter1 = task1.GetAwaiter();
-        awaiter1.OnCompleted(() =>
-        {
-            int result1 = awaiter1.GetResult();
-            Calculate2(result1);
-        });
+        //TaskAwaiter<int> awaiter1 = task1.GetAwaiter();
+        //awaiter1.OnCompleted(() =>
+        //{
+        //    int result1 = awaiter1.GetResult();
+        //    Calculate2(result1);
+        //});
 
-
+        await Calculate_1And2();
+        
         Calculate3();
 
     }
-
+   static async Task Calculate_1And2()
+        {
+            var result1 = await Task.Run(() =>
+            {
+                return Calculate1();
+            });
+            Calculate2(result1);
+        }
     private static int Calculate1()
     {
         Thread.Sleep(3000);
