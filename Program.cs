78f2dce6840812208fd03f5c0c0d62922c24c412
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-25 11:49:11 +0530

    OnCompleted() method of TaskAwaiter used

--- Program.cs
+++ Program.cs
@@ -6,30 +6,26 @@ class MyClass
     static void Main(string[] args)
     {
         Calculate();
+        Console.ReadLine();
     }
 
     private static void Calculate()
     {
-        Task<int> task1=Task.Run(() =>
+        Task<int> task1 = Task.Run(() =>
         {
             return Calculate1();
 
         });
 
-        Task<int> task2=Task.Run(() =>
+        TaskAwaiter<int> awaiter1 = task1.GetAwaiter();
+        awaiter1.OnCompleted(() =>
         {
-            return Calculate2();
-
+            int result1 = awaiter1.GetResult();
+            Calculate2(result1);
         });
-        
-        Task.WaitAll(task1,task2);
-        TaskAwaiter<int> awaiter1=task1.GetAwaiter();
-        TaskAwaiter<int> awaiter2=task2.GetAwaiter();
 
-        int result1=awaiter1.GetResult();
-        int result2=awaiter2.GetResult();
 
-        Calculate3(result1,result2);
+        Calculate3();
 
     }
 
@@ -40,15 +36,15 @@ class MyClass
         return 50;
     }
 
-    private static int Calculate2()
+    private static int Calculate2(int result1)
     {
         Console.WriteLine("Calculating result2");
-        return 150;
+        return 150+result1;
     }
 
-    private static int Calculate3(int result1,int result2)
+    private static int Calculate3()
     {
         Console.WriteLine("Calculating result3");
-        return result1+ result2;
+        return 179;
     }
 }
